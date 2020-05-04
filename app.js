b14f60ec18f694a2f45005f923e989468b114b83
Sibel <sbaslamisli@yahoo.com>
2020-05-04 00:55:05 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,121 @@
+//navbar 
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+    burger.addEventListener('click', () => {
+        //toogle nav
+
+        nav.classList.toggle('nav-active');
+        //animate links
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        //burger animation
+        burger.classList.toggle('toggle');
+    })
+}
+
+navSlide();
+
+
+// //animated mouse
+// let mouseCursor = document.querySelector('.newcursor');
+// let navLinks = document.querySelectorAll('.nav-links li');
+// let containers = document.querySelectorAll('.photoanimation img');
+
+// window.addEventListener('mousemove', cursor);
+
+// function cursor(e) {
+//     mouseCursor.style.top = e.pageY + "px";
+//     mouseCursor.style.left = e.pageX + "px";
+// }
+
+// navLinks.forEach(link => {
+//     link.addEventListener('mouseleave', () => {
+//         mouseCursor.classList.remove('link-grow');
+//     });
+//     link.addEventListener('mouseover', () => {
+//         mouseCursor.classList.add('link-grow');
+//     });
+// });
+
+// containers.forEach(img => {
+//     img.addEventListener('mouseleave', () => {
+//         mouseCursor.classList.remove('link-grow');
+//     });
+//     img.addEventListener('mouseleave', () => {
+//         mouseCursor.classList.add('link-grow');
+//     })
+// })
+
+
+//animated navbar
+
+const sections = document.querySelectorAll('section');
+const bubble = document.querySelector('.bubble');
+const colors = ['green', "green", "green", "green"];
+
+const options = {
+    threshold: 0.7
+};
+
+let observer = new IntersectionObserver(navCheck, options);
+
+function navCheck(entries) {
+    entries.forEach(entry => {
+        const className = entry.target.className;
+        const activeAnchor = document.querySelector(`[data-page=${className}]`);
+        const gradientIndex = entry.target.getAttribute('data-index');
+        const coords = activeAnchor.getBoundingClientRect();
+        const directions = {
+            height: coords.height,
+            width: coords.width,
+            top: coords.top,
+            left: coords.left
+        };
+        if (entry.isIntersecting) {
+            bubble.style.setProperty("left", `${directions.left}px`);
+            bubble.style.setProperty("top", `${directions.top}px`);
+            bubble.style.setProperty("width", `${directions.width}px`);
+            bubble.style.setProperty("height", `${directions.height}px`);
+            bubble.style.background = colors[gradientIndex];
+        }
+
+    });
+}
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+
+//typing
+const texts = ["HTML, CSS, JavaScript, React.Js!"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+    document.querySelector(".typing").textContent = letter;
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 400);
+
+}());
+
+AOS.init();
